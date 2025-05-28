7e5486347cfc63a33a81e1542e797c1ae6d170d5
amane217 <rimia0217@gmail.com>
2025-05-28 09:41:45 +0900

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,7 @@
+document.getElementById("official").addEventListener("mouseover", function() {
+    this.style.color = "#ff99cc";
+});
+
+document.getElementById("official").addEventListener("mouseout", function() {
+    this.style.color = "#ff3366";
+});
